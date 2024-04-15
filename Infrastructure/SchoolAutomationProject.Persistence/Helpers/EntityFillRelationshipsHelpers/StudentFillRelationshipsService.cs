05e2556f470a708db748cdfe78a18a87d7610461
yusuftataroglu <yusuftataroglu97@gmail.com>
2024-04-15 12:41:50 +0300

    updated StudentsController in Admin area

--- Infrastructure/SchoolAutomationProject.Persistence/Helpers/EntityFillRelationshipsHelpers/StudentFillRelationshipsService.cs
+++ Infrastructure/SchoolAutomationProject.Persistence/Helpers/EntityFillRelationshipsHelpers/StudentFillRelationshipsService.cs
@@ -4,6 +4,7 @@ using SchoolAutomationProject.Application.Repositories.AchievementRepositories;
 using SchoolAutomationProject.Application.Repositories.AttendanceRepositories;
 using SchoolAutomationProject.Application.Repositories.GradeRepositories;
 using SchoolAutomationProject.Application.Repositories.ParentRepositories;
+using SchoolAutomationProject.Application.Repositories.StudentRepositories;
 using SchoolAutomationProject.Application.ViewModels.AdminAreaViewModels.StudentViewModels;
 using SchoolAutomationProject.Domain.Entities.CrossTables;
 using SchoolAutomationProject.Domain.Entities.CustomTables;
@@ -19,17 +20,20 @@ namespace SchoolAutomationProject.Persistence.Helpers.EntityFillRelationshipsHel
         private readonly IParentReadRepository _parentReadRepository;
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<AppUserRole> _roleManager;
+        private readonly IStudentReadRepository _studentReadRepository;
 
         public StudentFillRelationshipsService(
             IParentWriteRepository parentWriteRepository,
             IParentReadRepository parentReadRepository,
             UserManager<AppUser> userManager,
-            RoleManager<AppUserRole> roleManager)
+            RoleManager<AppUserRole> roleManager,
+            IStudentReadRepository studentReadRepository)
         {
             _parentWriteRepository = parentWriteRepository;
             _parentReadRepository = parentReadRepository;
             _userManager = userManager;
             _roleManager = roleManager;
+            _studentReadRepository = studentReadRepository;
         }
         public async Task FillStudentRelationships(Student student, WriteStudentViewModel modelVM, string requestType)
         {
@@ -86,24 +90,15 @@ namespace SchoolAutomationProject.Persistence.Helpers.EntityFillRelationshipsHel
             }
             else if (requestType == "UpdatePost")
             {
-                //student.ParentId = modelVM.ParentId;
-                //student.Parent.FirstName = modelVM.ParentFirstName;
-                //student.Parent.LastName = modelVM.ParentLastName;
-                //student.Parent.MobilePhone = modelVM.ParentMobilePhone;
-                //student.Parent.WorkPhone = modelVM.ParentWorkPhone;
-                //student.Parent.Address = modelVM.ParentAddress;
-                //student.Parent.District = modelVM.ParentDistrict;
-                //student.Parent.City = modelVM.ParentCity;
+                var user = await _userManager.FindByNameAsync(student.User.UserName);
+                user.UserName = $"{modelVM.FirstName.ToLower().Trim().Replace(" ", "")}{modelVM.LastName.ToLower()}";
+                user.NormalizedUserName = $"{modelVM.FirstName.ToUpper().Trim().Replace(" ", "")}{modelVM.LastName.ToUpper()}";
+                user.Email = "";
+                await _userManager.RemovePasswordAsync(user);
+                await _userManager.AddPasswordAsync(user, $"{student.FirstName.ToLower().Trim().Replace(" ", "")}{student.LastName.ToLower()}");
             }
             else if (requestType == "UpdateGet")
             {
-                //modelVM.ParentFirstName = student.Parent.FirstName;
-                //modelVM.ParentLastName = student.Parent.LastName;
-                //modelVM.ParentMobilePhone = student.Parent.MobilePhone;
-                //modelVM.ParentWorkPhone = student.Parent.WorkPhone;
-                //modelVM.ParentAddress = student.Parent.Address;
-                //modelVM.ParentDistrict = student.Parent.District;
-                //modelVM.ParentCity = student.Parent.City;
             }
         }
     }
